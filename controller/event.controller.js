9f9daaa043bc372732f63a00a94ca2de52463225
antoinecaby <acaby@yipikay.fr>
2025-03-21 12:48:02 +0100

    a

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -1,12 +1,12 @@
 const Event = require("./../model/event.model.js");
 
 exports.create = async (req, res, next) => {
-  let Event = await Event.create(req.body.Event);
-  res.status(201).json(Event);
+  let event = await Event.create(req.body.Event);
+  res.status(201).json(event);
 };
 
 exports.getById = async (req, res, next) => {
   let id = req.params.id;
-  let Event = await Event.findByPk(id);
-  res.status(201).json(Event);
+  let event = await Event.findByPk(id);
+  res.status(201).json(event);
 };
